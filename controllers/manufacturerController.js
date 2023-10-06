052e38b0468c0d3867ea1c66ff18218b4f29b1b8
Brady <brady.savarie@hotmail.com>
2023-10-05 22:58:20 -0400

    complete manufacturer routes

---
+++ controllers/manufacturerController.js
@@ -0,0 +1,33 @@
+const asyncHandler = require('express-async-handler');
+
+exports.manufacturer_create_get = asyncHandler(async (req, res, next) => {
+    res.send('Manufacturer create get not yet implemented');
+});
+
+exports.manufacturer_create_post = asyncHandler(async (req, res, next) => {
+    res.send('Manufacturer create post not yet implemented');
+});
+
+exports.manufacturer_delete_get = asyncHandler(async (req, res, next) => {
+    res.send('Manufacturer delete get not yet implemented');
+});
+
+exports.manufacturer_delete_post = asyncHandler(async (req, res, next) => {
+    res.send('Manufacturer delete post not yet implemented');
+});
+
+exports.manufacturer_update_get = asyncHandler(async (req, res, next) => {
+    res.send('Manufacturer update get not yet implemented');
+});
+
+exports.manufacturer_update_post = asyncHandler(async (req, res, next) => {
+    res.send('Manufacturer update post not yet implemented');
+});
+
+exports.manufacturer_detail = asyncHandler(async (req, res, next) => {
+    res.send('Manufacturer detail not yet implemented');
+});
+
+exports.manufacturer_list = asyncHandler(async (req, res, next) => {
+    res.send('Manufacturer list not yet implemented');
+});
--- routes/catalog.js
+++ routes/catalog.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // require controller modules here //
 const effect_controller = require('../controllers/effectController');
 const effectInstance_controller = require('../controllers/effectInstanceController');
+const manufacturer_controller = require('../controllers/manufacturerController');
 
 // EFFECT ROUTES //
 
@@ -63,5 +64,41 @@ router.get(
 );
 
 router.get('/effectInstances', effectInstance_controller.effectInstance_list);
+
+// MANUFACTURER ROUTES //
+
+router.get(
+    '/manufacturer/create',
+    manufacturer_controller.manufacturer_create_get
+);
+
+router.post(
+    '/manufacturer/create',
+    manufacturer_controller.manufacturer_create_post
+);
+
+router.get(
+    '/manufacturer/delete',
+    manufacturer_controller.manufacturer_delete_get
+);
+
+router.post(
+    '/manufacturer/delete',
+    manufacturer_controller.manufacturer_delete_post
+);
+
+router.get(
+    '/manufacturer/update',
+    manufacturer_controller.manufacturer_update_get
+);
+
+router.post(
+    '/manufacturer/update',
+    manufacturer_controller.manufacturer_update_post
+);
+
+router.get('/manufacturer/:id', manufacturer_controller.manufacturer_detail);
+
+router.get('/manufacturers', manufacturer_controller.manufacturer_list);
 
 module.exports = router;
